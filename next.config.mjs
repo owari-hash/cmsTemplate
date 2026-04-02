fb35407999c6210ca448998b6a7edd9c2253f5c9
Anzai <55052716+owari-hash@users.noreply.github.com>
2026-04-02 22:04:58 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@cms-builder/core"],
+  experimental: {
+    allowedDevOrigins: ["202.179.6.77"],
+  },
 };
 
 export default nextConfig;
