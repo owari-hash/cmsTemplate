8f27e9a1e3410bafa4497615aae8c2d98daca5aa
Anzai <55052716+owari-hash@users.noreply.github.com>
2026-04-06 12:56:10 +0800

    z

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@cms-builder/core/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@cms-builder/core/dist/**/*.{js,ts,jsx,tsx,mjs}",
   ],
   darkMode: 'class',
   theme: {
